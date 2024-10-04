4d19fdd932ca9d490c4f3b1f3142198b5d8b6b09
Geoffrey <geoffreyleglise@gmail.com>
2024-10-04 13:27:19 +0200

    Exercice Conditions - Evaluer des variables

---
+++ index.js
@@ -0,0 +1,30 @@
+// Declare and define three variables using a numerical value
+
+// * If the three variables are equal, display "The three variables are the same" in the console
+// * If only two of the variables are equal, display "Two of the variables are the same" in the console
+// * If the variables are all different display "all variables are different" in the console
+
+const a = 1;
+const b = 2;
+const c = 2;
+
+if ( a === b && b === c ){
+    console.log("les 3 valeurs sont identique")
+} else if ( a === b || a === c || b === c ) {
+    console.log("les 2 valeurs sont identique")
+}else {
+    console.log("les 3 valeurs sont differente")
+}
+
+
+
+
+
+
+
+//*Déclare et définis trois variables à l'aide d'une valeur numérique.
+//(ex : const a = 1)
+
+//Si les trois variables sont égales, affiche "Les trois variables sont identiques" dans la console
+//Si seulement deux des variables sont égales, affiche "Deux des variables sont identiques" dans la console
+//Si les variables sont toutes différentes, affiche "toutes les variables sont différentes" dans la console.
